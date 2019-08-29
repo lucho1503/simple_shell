c007fa6af1543436cf7f4c8351f0aabbf9b39705
Alvaro Contreras <alvaro.contreras.academico@gmail.com>
2019-08-29 21:40:20 +0000

    update printenv.c

--- printenv.c
+++ printenv.c
@@ -1,5 +1,11 @@
 #include "holberton.h"
 
+/**
+ * count_dir - count the files in the path
+ * @path: path
+ * Return: dir
+ */
+
 unsigned int count_dir(char *path)
 {
 	unsigned int dir = 0, count = 0, flag = 0;
@@ -17,27 +23,45 @@ unsigned int count_dir(char *path)
 	return (dir);
 }
 
-char *_strncpycmd(char *dest, char *src, char* com, int n, int c)
+/**
+ * _strncpycmd - entry point
+ * @dest: dest
+ * @src: src
+ * @com: com
+ * @n: n
+ * @c: c
+ * Return: dest
+ */
+
+char *_strncpycmd(char *dest, char *src, char *com, int n, int c)
 {
-  int i, j;
+	int i, j;
 
-  i = 0, j = 0;
-  while (i < n && src[i] != '\0')
-    {
-      dest[i] = src[i];
-      i++;
-    }
-  dest[i] = '/';
-  i++;
+	i = 0, j = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '/';
+	i++;
 
-  while (j < c && com[j] != '\0')
-    {
-      dest[i] = com[j];
-      j++; i++;
-    }
-  dest[i] = '\0';
-  return (dest);
+	while (j < c && com[j] != '\0')
+	{
+		dest[i] = com[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
+
+/**
+ * store_environ - entry point
+ * @com: com
+ * @environ: enviaron
+ * Return: dris
+ */
 
 char **store_environ(char *com, char **environ)
 {
@@ -45,7 +69,7 @@ char **store_environ(char *com, char **environ)
 	char *path, *dir;
 	unsigned int largo, count;
 	int dir_len, len_com;
-	
+
 	path = _getenv("PATH", environ);
 	largo = count_dir(path);
 	dirs = malloc(sizeof(char *) * (largo + 1));
@@ -68,6 +92,13 @@ char **store_environ(char *com, char **environ)
 	dirs[count] = NULL;
 	return (dirs);
 }
+
+/**
+ * _getenv - entry point
+ * @name: name
+ * @environ: enviaron
+ * Return: NULL
+ */
 
 char *_getenv(const char *name, char **environ)
 {
@@ -97,10 +128,15 @@ char *_getenv(const char *name, char **environ)
 	return (NULL);
 }
 
+/**
+ * print_env - print enviaron variables
+ * @environ: environ
+ */
+
 void print_env(char **environ)
 {
 	int count, len;
-	
+
 	for (count = 0; environ[count]; ++count)
 	{
 		len = _strlen(environ[count]);
@@ -108,4 +144,3 @@ void print_env(char **environ)
 		write(STDOUT_FILENO, "\n", 1);
 	}
 }
-
