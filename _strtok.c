09dacf0ad98556633309888dbc24047d777497ad
Doniben <doniben@esperanto.co>
2019-08-29 22:12:07 +0000

    fix

--- _strtok.c
+++ _strtok.c
@@ -1,5 +1,11 @@
 #include "holberton.h"
 
+/**
+ * largo_del_comando - entry point.
+ * @s: string.
+ * Return: lenght of command.
+ */
+
 unsigned int largo_del_comando(char *s)
 {
 	unsigned int commands, i, flag;
@@ -22,37 +28,42 @@ unsigned int largo_del_comando(char *s)
 	return (commands);
 }
 
+/**
+ * _strtok - entry point.
+ * @str: string recived.
+ * Return: double pointer in tokens.
+ */
+
 char **_strtok(char *str)
 {
-	char **tk_hol;
-	char *token;
-	int i;
-	unsigned int len;
-
-	str[_strlen(str) - 1] = '\0';
-	len = largo_del_comando(str);
-	if (len == 0)
-		return (NULL);
-        
-	tk_hol = malloc((sizeof(char *)) * (len + 1));
-	if (tk_hol == NULL)
+  char **tk_hol;
+  char *token;
+  int i;
+  unsigned int len;
+  
+  str[_strlen(str) - 1] = '\0';
+  len = largo_del_comando(str);
+  if (len == 0)
+    return (NULL);        
+  tk_hol = malloc((sizeof(char *)) * (len + 1));
+  if (tk_hol == NULL)
+    {
+      return (NULL);
+    }
+  i = 0;
+  token = strtok(str, " ");
+  while (token != NULL)
+    {
+      tk_hol[i] = malloc(_strlen(token) + 1);
+      if (tk_hol[i] == NULL)
 	{
-		return (NULL);
+	  free_all(tk_hol);
+	  return (NULL);
 	}
-	i = 0;
-	token = strtok(str, " ");
-	while (token != NULL)
-	{
-		tk_hol[i] = malloc(_strlen(token) + 1);
-		if (tk_hol[i] == NULL)
-		{
-			free_all(tk_hol);
-			return (NULL);
-		}
-		_strncpy(tk_hol[i], token, _strlen(token) + 1);
-		token = strtok(NULL, " ");
-		++i;
-	}
-	tk_hol[i] = NULL;
-	return (tk_hol);
+      _strncpy(tk_hol[i], token, _strlen(token) + 1);
+      token = strtok(NULL, " ");
+      ++i;
+    }
+  tk_hol[i] = NULL;
+  return (tk_hol);
 }
