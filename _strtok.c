b17f56170497304840258eb4aa1e70758d1f64b6
Alvaro Contreras <alvaro.contreras.academico@gmail.com>
2019-08-29 21:58:02 +0000

    update _strtok.c

--- _strtok.c
+++ _strtok.c
@@ -36,34 +36,34 @@ unsigned int largo_del_comando(char *s)
 
 char **_strtok(char *str)
 {
-  char **tk_hol;
-  char *token;
-  int i;
-  unsigned int len;
-  
-  str[_strlen(str) - 1] = '\0';
-  len = largo_del_comando(str);
-  if (len == 0)
-    return (NULL);        
-  tk_hol = malloc((sizeof(char *)) * (len + 1));
-  if (tk_hol == NULL)
-    {
-      return (NULL);
-    }
-  i = 0;
-  token = strtok(str, " ");
-  while (token != NULL)
-    {
-      tk_hol[i] = malloc(_strlen(token) + 1);
-      if (tk_hol[i] == NULL)
+	char **tk_hol;
+	char *token;
+	int i;
+	unsigned int len;
+
+	str[_strlen(str) - 1] = '\0';
+	len = largo_del_comando(str);
+	if (len == 0)
+		return (NULL);
+	tk_hol = malloc((sizeof(char *)) * (len + 1));
+	if (tk_hol == NULL)
 	{
-	  free_all(tk_hol);
-	  return (NULL);
+		return (NULL);
 	}
-      _strncpy(tk_hol[i], token, _strlen(token) + 1);
-      token = strtok(NULL, " ");
-      ++i;
-    }
-  tk_hol[i] = NULL;
-  return (tk_hol);
+	i = 0;
+	token = strtok(str, " ");
+	while (token != NULL)
+	{
+		tk_hol[i] = malloc(_strlen(token) + 1);
+		if (tk_hol[i] == NULL)
+		{
+			free_all(tk_hol);
+			return (NULL);
+		}
+		_strncpy(tk_hol[i], token, _strlen(token) + 1);
+		token = strtok(NULL, " ");
+		++i;
+	}
+	tk_hol[i] = NULL;
+	return (tk_hol);
 }
